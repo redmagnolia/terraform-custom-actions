c0c67caad8387678ef5ca0bf78a590febde0dbe0
Fadil <fadil2610@gmail.com>
2022-03-13 15:18:56 +0100

    Log user

--- github-issue-comment/index.js
+++ github-issue-comment/index.js
@@ -17,7 +17,7 @@ const github = require('@actions/github');
                 issue_number: context.issue.number,
             });
     
-            console.log('Old comments: ', oldComments);
+            console.log('Old comments: ', oldComments.data[0].user);
 
             await octokit.rest.issues.createComment({
                 owner: context.repo.owner,
